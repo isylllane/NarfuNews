27fe288d41d1f49a32d10292a75d80a1f3cf7ef5
Isyllane <lutik301@gmail.com>
2026-04-18 19:14:24 +0300

    Исправление найденные в лабе 5

--- Frontend/src/views/LastNews.vue
+++ Frontend/src/views/LastNews.vue
@@ -21,7 +21,42 @@ const handleNewsClick = (news) => {
   console.log('Клик по новости:', news)
 }
 </script>
-
 <template>
-Последние новости
+  <v-row>
+    <v-col>
+      <div class="text-md-headline-medium text-headline-small font-weight-bold text-primary mb-6">
+        Последние новости
+      </div>
+    </v-col>
+  </v-row>
+  <!-- Главная новость -->
+  <v-sheet class="bg-white mb-8 pa-md-4 pa-2">
+    <v-row v-if="mainNews">
+      <v-col>
+        <MainNews :news="mainNews" />
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col
+          v-for="news in otherNews"
+          :key="news.id"
+          cols="12"
+          sm="6"
+          md="4"
+          lg="3"
+      >
+        <NewsCard :news="news" @click="handleNewsClick(news)" />
+      </v-col>
+    </v-row>
+
+    <v-row v-if="!otherNews.length">
+      <v-col>
+        <div class="text-center text-grey pa-8">
+          <v-icon icon="mdi-newspaper-variant-outline" size="48" class="mb-2"></v-icon>
+          <div>Нет новостей</div>
+        </div>
+      </v-col>
+    </v-row>
+  </v-sheet>
 </template>
