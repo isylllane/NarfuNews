8308302a7f6fa1d5f387722a4cc0bf14c7f148e4
Isyllane <lutik301@gmail.com>
2026-04-18 17:43:52 +0300

    измен

--- Frontend/src/views/LastNews.vue
+++ Frontend/src/views/LastNews.vue
@@ -23,44 +23,5 @@ const handleNewsClick = (news) => {
 </script>
 
 <template>
-    <v-row>
-      <v-col>
-        <div class="text-h3 font-weight-bold text-primary mb-6">
-          Последние новости
-        </div>
-      </v-col>
-    </v-row>
-
-    <!-- Главная новость -->
-    <v-sheet class="bg-white mb-8 pa-md-4 pa-2">
-      <v-row v-if="mainNews">
-        <v-col>
-          <MainNews :news="mainNews" />
-        </v-col>
-      </v-row>
-
-      <v-row>
-        <v-col
-            v-for="news in otherNews"
-            :key="news.id"
-            cols="12"
-            sm="6"
-            md="4"
-            lg="3"
-        >
-          <NewsCard :news="news" @click="handleNewsClick(news)" />
-        </v-col>
-      </v-row>
-
-      <v-row v-if="!otherNews.length">
-        <v-col>
-          <div class="text-center text-grey pa-8">
-            <v-icon icon="mdi-newspaper-variant-outline" size="48" class="mb-2"></v-icon>
-            <div>Нет новостей</div>
-          </div>
-        </v-col>
-      </v-row>
-    </v-sheet>
-
-
+Последние новости
 </template>
